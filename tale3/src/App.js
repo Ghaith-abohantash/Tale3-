df131bcd01789ee6f83f25649e9b344cf435b3e0
ghaith <s12011251@stu.najah.edu>
2024-05-07 21:22:46 +0300

    editing

--- tale3/src/App.js
+++ tale3/src/App.js
@@ -1,21 +1,13 @@
-import BusScheduleTable from "./components/Bus schedule/schedule";
-import Footer from "./components/Footer/footer";
-import LoginForm from "./components/log in form/log";
 
 function App() {
-  const buses = [
-    { destination: 'الأكاديمية الى الجامعة القديمة', ticketPrice: 2.5 , passengerCount: 20, emptySeats: 10, departureTime: '10:00 ص' },
-    { destination: 'الأكاديمية الى المجمع الغربي', ticketPrice: 3, passengerCount: 15, emptySeats: 5, departureTime: '11:00 ص' },
-    { destination: 'الأكاديمية إلى الدوار', ticketPrice: 4, passengerCount: 25, emptySeats: 15, departureTime: '12:00 ظ' },
-  ];
-
   return (
-    <div className="app">
-      
-      <BusScheduleTable buses={buses} />
-      <Footer/>
-    </div>
+   <div> 
+
+
+    
+   </div>
+  
   );
-};
+}
 
 export default App;
