9eb44f2297efd573ad400f1b75cf12540e125ff8
wafaahmad <s12144189@stu.najah.edu>
2024-05-30 23:14:02 +0300

    final sub

---
+++ backend/models/Booking.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const BookingSchema = new mongoose.Schema({
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  numberOfPeople: {
+    type: Number,
+    required: true,
+  },
+  cardDetails: {
+    number: 
+    { 
+      type: String 
+    },
+    expiry: { 
+      type: String 
+    },
+    cvc: { 
+      type: String 
+    },
+  },
+  busDestination: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Book = mongoose.model("Booking", BookingSchema);
+
+export default Book;
